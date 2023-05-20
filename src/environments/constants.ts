cd1825888fbc0009ef6f33f11870e514dac8f74d
Paulo Silva <paulo.silva@celk.net>
2023-05-20 12:18:52 -0300

    create folder with constant

---
+++ src/environments/constants.ts
@@ -0,0 +1,10 @@
+import { ModuleWithProviders } from "@angular/core";
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+/** Usage to Enable DevTools to see Store Redux */
+export const storeTools: Array<ModuleWithProviders<any>> = [
+    StoreDevtoolsModule.instrument({
+        maxAge: 25,
+        name: 'Meu Time'
+    })
+  ];
